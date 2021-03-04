97d0a37a0b401822912c83360ea06feb4cf3e9ff
Jibidus <jibidus@gmail.com>
2021-03-04 17:11:49 +0100

    :gear: setup frontend

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    plugins: ["jest-dom"],
+    extends: ["plugin:jest-dom/recommended", "eslint:recommended", "plugin:vue/recommended"]
+    // See for jest-dom rules: https://github.com/testing-library/eslint-plugin-jest-dom
+};
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: '@vue/cli-plugin-unit-jest',
+    testMatch: ["**/spec/javascripts/**/*.spec.js"],
+    roots: ['<rootDir>'],
+    moduleNameMapper: {
+        "^@\/(.*)$": "<rootDir>/app/javascript/$1"
+    },
+    resolver: null,
+    setupFilesAfterEnv: ['./spec/support/jest-setup.js'],
+    collectCoverageFrom: ["app/javascript/**/*.{js,vue}"],
+    coverageDirectory: "coverage/frontend"
+}
