862aad6a681b58baa7f991d3da33ae1f6bb8b041
Jibidus <jibidus@gmail.com>
2021-03-05 20:40:35 +0100

    :gear: add eslint rules for testing-library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    plugins: ["jest-dom", "jest"],
-    extends: ["plugin:jest-dom/recommended", "eslint:recommended", "plugin:vue/recommended"],
+    plugins: ["jest-dom", "jest", "testing-library"],
+    extends: ["plugin:jest-dom/recommended", "eslint:recommended", "plugin:vue/recommended", "plugin:testing-library/recommended", "plugin:testing-library/vue"],
     env: {
         "jest/globals": true
     }
