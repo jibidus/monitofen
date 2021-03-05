499b771fbab8a0a99a6b3039c07b7fb7cf1d4a2b
Jibidus <jibidus@gmail.com>
2021-03-05 20:30:11 +0100

    :gear: configure eslint with jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-    plugins: ["jest-dom"],
-    extends: ["plugin:jest-dom/recommended", "eslint:recommended", "plugin:vue/recommended"]
-    // See for jest-dom rules: https://github.com/testing-library/eslint-plugin-jest-dom
+    plugins: ["jest-dom", "jest"],
+    extends: ["plugin:jest-dom/recommended", "eslint:recommended", "plugin:vue/recommended"],
+    env: {
+        "jest/globals": true
+    }
 };
