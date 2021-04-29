2f1d6d04934b0b963f959c4a53be4c91e640fb51
Jibidus <jibidus@gmail.com>
2021-04-29 21:49:05 +0200

    :gear: add time period parameter to GET /measures endpoint

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,7 +1,18 @@
 class MeasuresController < ApplicationController
   def index
     metric = Metric.find(params[:metric_id])
-    measures = Measure.select(metric)
-    render json: measures
+    json = Measure.where(date: from_param..to_param)
+                  .map { |m| { id: m.id, date: m.date, value: m[metric.column_name] } }
+    render json: json
+  end
+
+  private
+
+  def from_param
+    params[:from].to_datetime
+  end
+
+  def to_param
+    params[:to].to_datetime
   end
 end
