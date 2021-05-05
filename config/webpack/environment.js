c95fa87a0980b45507991b3d841c1564fe2a7d48
Jibidus <jibidus@gmail.com>
2021-05-05 22:18:07 +0200

    :gear: limit the amount of loaded timezones in the browser

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const MomentTimezoneDataPlugin = require('moment-timezone-data-webpack-plugin');
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.plugins.prepend('MomentTimezoneDataPlugin', new MomentTimezoneDataPlugin({matchZones: /^Europe\/Paris/}))
 environment.loaders.prepend('vue', vue)
 environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' };
 module.exports = environment
