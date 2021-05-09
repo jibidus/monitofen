ee8d81ced863a812ac80ac29eed8415e5405eff9
Jibidus <jibidus@gmail.com>
2021-05-09 15:14:56 +0200

    :gear: extract routes declarations in a dedicated file

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,23 +1,15 @@
 import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import VueRouter from 'vue-router'
-import HomePage from "../HomePage";
-import MeasuresPage from "../MeasuresPage";
 import vuetify from '../plugins/vuetify';
 import App from '../App';
 import moment from "moment-timezone";
+import routes from "../routes";
 
 moment.tz.setDefault("Europe/Paris");
 Vue.use(TurbolinksAdapter);
 document.addEventListener('turbolinks:load', () => {
     Vue.use(VueRouter);
-    const routes = [
-        {name: 'home', path: '/', component: HomePage},
-        {name: 'measures', path: '/measures', component: MeasuresPage},
-    ]
-    const router = new VueRouter({
-        routes
-    })
-
+    const router = new VueRouter({routes})
     new Vue({router, vuetify, render: h => h(App)}).$mount('#app');
 });
---
+++ app/javascript/routes.js
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import MeasuresPage from "./MeasuresPage";
+
+export default [
+    {name: 'home', path: '/', component: HomePage},
+    {name: 'measures', path: '/measures', component: MeasuresPage},
+]
