06eed91f0b844e09f3f87e7f3b47589853f1a6ba
Jibidus <jibidus@gmail.com>
2021-05-09 14:02:12 +0200

    :bug: fix styling in production

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'vuetify/dist/vuetify.min.css'
 
 Rails.start()
 Turbolinks.start()
---
+++ app/javascript/plugins/vuetify.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+const opts = {icons: {iconfont: 'fa'}}
+export default new Vuetify(opts)
