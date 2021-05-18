0061db9c91835fb19ad70f84678170d3e134e913
Jibidus <jibidus@gmail.com>
2021-05-18 21:02:09 +0200

    :gear: refactor some tests

--- spec/javascripts/components/MeasuresChartWrapper.spec.js
+++ spec/javascripts/components/MeasuresChartWrapper.spec.js
@@ -4,13 +4,11 @@ import {byTestId, byText} from 'testing-library-selector'
 import {screen} from '@testing-library/vue';
 import buildMetric from '../factories/metric';
 import buildMeasure from '../factories/measure';
+import buildResponse from "../factories/http_response";
 import moment from "moment-timezone";
 import MockDate from 'mockdate'
 import axios from "axios";
 import {DEFAULT_TIMEZONE} from "../../support/constants";
-
-jest.mock('axios');
-beforeEach(() => axios.get.mockClear());
 
 const ui = {
     placeholder: byText('No measure'),
@@ -20,7 +18,7 @@ const ui = {
 describe("<MeasuresChartWrapper/>", function () {
     describe('without any measure', () => {
         beforeEach(async () => {
-            axios.get.mockResolvedValue({data: [], status: 200});
+            axios.get.mockResolvedValue(buildResponse.Successful([]));
             await renderCmp(MeasuresChartWrapper, {props: {metric: buildMetric()}});
         });
         it('shows placeholder', () => {
@@ -30,17 +28,14 @@ describe("<MeasuresChartWrapper/>", function () {
     describe('with measures', () => {
         beforeEach(async () => {
             MockDate.set('2021-02-16T22:26:51+01:00');
-            axios.get.mockResolvedValue({data: [buildMeasure(), buildMeasure(), buildMeasure()], status: 200});
+            axios.get.mockResolvedValue(buildResponse.Successful([buildMeasure(), buildMeasure(), buildMeasure()]));
             await renderCmp(MeasuresChartWrapper, {props: {metric: buildMetric()}});
             MockDate.reset();
         });
         it('requests measures for last 24h', () => {
-            expect(axios.get.mock.calls[0][1]).toStrictEqual({
-                params: {
-                    from: "2021-02-15 22:26:51 +0100",
-                    to: "2021-02-16 22:26:51 +0100"
-                }
-            });
+            let firstParamCall = axios.get.mock.calls[0][1].params;
+            expect(firstParamCall.from).toEqual("2021-02-15 22:26:51 +0100");
+            expect(firstParamCall.to).toEqual( "2021-02-16 22:26:51 +0100");
         });
         it('does not show placeholder', () => {
             expect(ui.placeholder.query()).toBeNull();
@@ -54,16 +49,13 @@ describe("<MeasuresChartWrapper/>", function () {
     });
     describe('when time period is given', () => {
         it('fetches measures with this time period', async () => {
-            axios.get.mockResolvedValue({data: [buildMeasure()], status: 200});
+            axios.get.mockResolvedValue(buildResponse.Successful([buildMeasure()]));
             const from = moment.tz("2020-04-10T10:20:30+02:00", DEFAULT_TIMEZONE);
             const to = moment.tz("2020-04-11T10:20:30+02:00", DEFAULT_TIMEZONE);
             await renderCmp(MeasuresChartWrapper, {props: {metric: buildMetric(), from, to}});
-            expect(axios.get.mock.calls[0][1]).toStrictEqual({
-                params: {
-                    from: "2020-04-10 10:20:30 +0200",
-                    to: "2020-04-11 10:20:30 +0200"
-                }
-            });
+            let firstParamCall = axios.get.mock.calls[0][1].params;
+            expect(firstParamCall.from).toEqual("2020-04-10 10:20:30 +0200");
+            expect(firstParamCall.to).toEqual( "2020-04-11 10:20:30 +0200");
         });
     });
 });
--- spec/javascripts/factories/http_response.js
+++ spec/javascripts/factories/http_response.js
@@ -1,1 +1,7 @@
-export default {Successful: (data) => ({status: 200, data})};
+export default {
+    Successful(data) {
+        return {status: 200, data};
+    }, Forbidden() {
+        return {status: 403};
+    }
+};
