fabddb68f5d73811f2045224b2519292cf49a9f4
Jibidus <jibidus@gmail.com>
2021-11-10 18:17:34 +0100

    :gear: make SMTP server configurable

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
-  :password => ENV['SENDGRID_API_KEY'],
-  :domain => 'jibidus.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :user_name => ENV['SMTP_USERNAME'],
+  :password => ENV['SMTP_PASSWORD'],
+  :domain => ENV['SMTP_DOMAIN'],
+  :address => ENV['SMTP_HOST'],
+  :port => ENV['SMTP_PORT'],
   :authentication => :plain,
   :enable_starttls_auto => true
 }
